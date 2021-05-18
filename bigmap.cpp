291afa983769b70b3415ff0b8b6338070627934c
John Aegard <jaegard@marchex.com>
2021-05-17 23:08:40 -0700

    boinging within viewport

--- bigmap.cpp
+++ bigmap.cpp
@@ -94,11 +94,16 @@ void BigMapEngine::render_viewport(Viewport* viewport, bool _render) {
 
   uint16_t row1   = viewport->inner_y_offset_px / tilelist->tile_size_px;
   uint16_t height = viewport->h_px/tilelist->tile_size_px;
-  uint16_t row2   = row1+height + ((voff > 1) ? 1 : 0); // render a bonus row if voff is off-zero
+  uint16_t row2   = row1+height+1; 
 
-  if (framecounter % 400 == 0) {
+  uint16_t crop_top =   viewport->y_px;
+  uint16_t crop_bottom = viewport->y_px + viewport->h_px -1;
+
+  if (framecounter % 600 == 0) {
     Serial.print("frame=");
     Serial.print(framecounter);
+    Serial.print(" height=");
+    Serial.print(height);
     Serial.print(" inner_yoff=");
     Serial.print(viewport->inner_y_offset_px);
     Serial.print(" voff=");
@@ -113,15 +118,19 @@ void BigMapEngine::render_viewport(Viewport* viewport, bool _render) {
     Serial.println(row2);
   }
 
-  for(uint8_t r=row1; r<row2; r++) {
+  for(uint8_t r=row1; r<=row2; r++) {
 
     int16_t line = ((r-row1) * tilelist->tile_size_px) - voff;
 
-    if (framecounter % 400 == 0) {
-      Serial.print("rendering row=");
+    if (framecounter % 600 == 0) {
+      Serial.print("rendering maprow=");
       Serial.print(r);
-      Serial.print(" line=");
-      Serial.println(line);
+      Serial.print(" screen line=");
+      Serial.print(line);
+      Serial.print(" crop_top=");
+      Serial.print(crop_top);
+      Serial.print(" crop_bottom=");
+      Serial.println(crop_bottom);
     }
     for(uint8_t c=col1; c<col2; c++) {
       uint8_t tile_index = viewport->tilemap->get_tile_index(c,r); 
@@ -130,85 +139,12 @@ void BigMapEngine::render_viewport(Viewport* viewport, bool _render) {
         tilelist->tile_size_px,
         viewport->x_px + ((c-col1) * tilelist->tile_size_px), 
         line,
-        viewport->y_px,
-        viewport->y_px + viewport->h_px -1 ,
-        framecounter % 400 == 0 && (r==row2-1 || r==row1) && c==0,
+        crop_top,
+        crop_bottom,
+        framecounter % 600 == 0 && (r==row2-1 || r==row1) && c==0,
         _render 
       );
     } 
   }
 }
-
-//void VGA_T4::run_bigmap_engine()
-//{
-//  waitLine(480+40);
-//
-//  unsigned char * tilept;
-//
-//  // Layer 0
-//  for (int j=0; j<TILES_ROWS; j++)
-//  {
-//    tilept = &tilesram[j*TILES_COLS];
-//    if ( (j>=hscr_beg[0]) && (j<=hscr_end[0]) ) {     
-//      int modcol = (hscr[0] >> TILES_HBITS) % TILES_COLS;
-//      for (int i=0; i<TILES_COLS; i++)
-//      {
-//        (i == 0) ? drawTileCropL(tilept[modcol], (i<<TILES_HBITS) - (hscr[0] & TILES_HMASK), j*TILES_H) : 
-//          (i == (TILES_COLS-1))?drawTileCropR(tilept[modcol], (i<<TILES_HBITS) - (hscr[0] & TILES_HMASK), j*TILES_H) : 
-//            drawTile(tilept[modcol], (i<<TILES_HBITS) - (hscr[0] & TILES_HMASK), j*TILES_H);
-//        modcol++;
-//        modcol = modcol % TILES_COLS; 
-//      }
-//    }  
-//    else {
-//      for (int i=0; i<TILES_COLS; i++)
-//      {
-//        (i == (TILES_COLS-1)) ? drawTileCropR(tilept[i], (i<<TILES_HBITS), j*TILES_H) :
-//          drawTile(tilept[i], (i<<TILES_HBITS), j*TILES_H);
-//      }      
-//    }
-//  }
-//
-//  // Other layers
-//  if (nb_layers > 1) {
-//    int lcount = 1;
-//    while (lcount < nb_layers) {
-//      for (int j=0; j<TILES_ROWS; j++)
-//      {
-//        tilept = &tilesram[(j+lcount*TILES_ROWS)*TILES_COLS];
-//        if ( (j>=hscr_beg[lcount]) && (j<=hscr_end[lcount]) ) {     
-//          int modcol = (hscr[lcount] >> TILES_HBITS) % TILES_COLS;
-//          for (int i=0; i<TILES_COLS; i++)
-//          {
-//            (i == 0) ? drawTransTileCropL(tilept[modcol], (i<<TILES_HBITS) - (hscr[lcount] & TILES_HMASK), j*TILES_H) : 
-//              (i == (TILES_COLS-1))?drawTransTileCropR(tilept[modcol], (i<<TILES_HBITS) - (hscr[lcount] & TILES_HMASK), j*TILES_H) : 
-//                drawTransTile(tilept[modcol], (i<<TILES_HBITS) - (hscr[lcount] & TILES_HMASK), j*TILES_H);
-//            modcol++;
-//            modcol = modcol % TILES_COLS; 
-//          }
-//        }          
-//        else {
-//          for (int i=0; i<TILES_COLS; i++)
-//          {
-//            drawTransTile(tilept[i], (i<<TILES_HBITS), j*TILES_H);
-//          }      
-//        }
-//      }
-//      lcount++;
-//    }  
-//  } 
-//
-///*
-// static char * ro="01234567890123456789012345678901234567";
-// for (int i=0; i<TILES_ROWS*2; i++)
-// {
-//  tileTextOverlay(0, i*8, ro, VGA_RGB(0x00,0xff,0x00)); 
-// }
-//*/
-//
-// for (int i=0; i<SPRITES_MAX; i++)
-// {
-//   drawSpr(spritesdata[i].index, spritesdata[i].x, spritesdata[i].y);
-// }
-//}
 
